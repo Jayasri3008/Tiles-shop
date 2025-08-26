4be81ca54e72ee483f97b2946947a7176e8ca6bf
Jayasri3008 <148859997+Jayasri3008@users.noreply.github.com>
2025-08-26 21:47:01 +0530

    Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,46 @@ import { Link } from "react-router-dom";
 
 export default function Navbar({ cartCount }) {
   return (
-    <nav className="nav">
-      <div className="brand">JAI Tiles🏭</div>
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/cart">🛒({cartCount})</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+      <div className="container">
+        {/* Brand */}
+        <Link className="navbar-brand fw-bold" to="/">
+          JAI Tiles 🏭
+        </Link>
+
+        {/* Toggler (mobile menu) */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/products">
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cart">
+                🛒 Cart ({cartCount})
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
