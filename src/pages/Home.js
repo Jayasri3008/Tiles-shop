1dc4b3efa442051bf1b0068ebb3a181751b069c6
Jayasri3008 <148859997+Jayasri3008@users.noreply.github.com>
2025-08-26 21:47:39 +0530

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 export default function Home() {
   return (
-    <div className="hero">
-      <h1>Welcome to JAI Tiles🏬 </h1>
-      <p>"Design your Floor.Define your Home"</p>
-      
+    <div className="container my-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+          <div className="hero card text-center shadow-lg p-5">
+            <h1 className="display-4 text-danger fw-bold">
+              Welcome to JAI Tiles 
+            </h1>
+            <p className="lead text-success fst-italic">
+              "Design your Floor. Define your Home"
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
